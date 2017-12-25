2b830ede1e4e8894c4be0c2da837480786262d71
EvilPomelo <123227682@qq.com>
2017-12-25 16:18:51 +0800

    Init

---
+++ Assets/SUIFW/Config/SysDefins.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SUIFW
+{
+    #region 系统枚举类型
+    /// <summary>
+    /// UI窗体(位置)类型
+    /// </summary>
+    public enum UIFormType
+    {
+        //普通窗体，
+        Normal,
+        //固定窗体，
+        Fixed,
+        //弹出窗体
+        PopUp
+    }
+    /// <summary>
+    /// UI窗体显示类型
+    /// </summary>
+    public enum UIFormShowMode
+    {
+        //普通窗体
+        Normal,
+        //固定窗体
+        Fixed,
+        //弹出窗体
+        PopUp,
+    }
+    #endregion
+}
